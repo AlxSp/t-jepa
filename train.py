0a255d4f881ecfa84490284f8a04da21c3499b9a
Alex Speicher <alexanderspeicher@gmail.com>
2023-07-15 19:50:47 -0400

    clean up

--- train.py
+++ train.py
@@ -93,19 +93,19 @@ for index, sample in enumerate(tqdm(dataset['train'])):
 
     block_size = int(target_embeddings.shape[1] * mask_ratio / target_block_num)
     print(block_size)
-    target_block_indices = torch.randint(0, target_embeddings.shape[1] - block_size, (target_block_num,))   
-    target_block_ranges = torch.stack([target_block_indices, target_block_indices + block_size], dim = 1)
+    target_block_ranges = torch.randint(0, target_embeddings.shape[1] - block_size, (target_block_num,))   
+    target_block_indices = torch.stack([target_block_ranges, target_block_ranges + block_size], dim = 1)
 
-    target_blocks_embeddings = [target_embeddings[target_block_range[0]:target_block_range[1]] for target_block_range in target_block_ranges]
+    target_blocks_embeddings = [target_embeddings[target_block_range[0]:target_block_range[1]] for target_block_range in target_block_indices]
 
     masked_ids = torch.clone(tokenized['input_ids'])
     # target_block_mask = torch.zeros((target_embeddings.shape[1]), dtype = torch.bool)
-    for target_index, target_block_range in enumerate(target_block_ranges):
+    for target_index, target_block_range in enumerate(target_block_indices):
         masked_ids[0,target_block_range[0]:target_block_range[1]] = tokenizer.unk_token_id
 
     print(sum(masked_ids[0] == tokenizer.unk_token_id) / masked_ids.shape[1])
 
-    print(target_block_ranges)
+    print(target_block_indices)
     print(tokenizer.decode(tokenized['input_ids'][0]))
     print(tokenizer.decode(masked_ids[0]))
     # target_block_mask = torch.zeros(target_embeddings.shape[1], dtype = torch.bool)
@@ -137,42 +137,28 @@ tokenized = tokenizer(dataset['train'][1:3]['text'], padding = True, truncation 
 true_input_lengths = torch.sum(tokenized['attention_mask'], dim = 1) # get the true input length for each sample
 min_length = torch.min(true_input_lengths) # get the length of the smallest sample
 block_size = int(min_length * mask_ratio / target_block_num) # compute the block size based on the mask ratio and smallest input #number of inputs
-target_block_indices = torch.cat([torch.randint(0, high  - block_size, (target_block_num,)) for high in true_input_lengths])# select target block indices
+target_block_ranges = torch.cat([torch.randint(0, high  - block_size, (target_block_num,)) for high in true_input_lengths])# select target block ranges for each target block
 #%%
 # compute the target embeddings
 target_embeddings = target_encoder(tokenized['input_ids']) # get target embeddings, no need to provide input indices.
 
 #%%
-# create the ranges for the target blocks
-target_block_ranges = torch.stack([torch.arange(block_start, block_start + block_size) for block_start in target_block_indices]).view(target_embeddings.shape[0], target_block_num, block_size) 
-
-target_blocks_embeddings = torch.stack([target_embeddings[index,target_block_range,:] for index, target_block_range in enumerate(target_block_ranges)]) # get the target embeddings
-target_blocks_embeddings.shape
+# create the indices for the target blocks
+target_block_indices = torch.stack([torch.arange(block_start, block_start + block_size) for block_start in target_block_ranges]).view(target_embeddings.shape[0], target_block_num, block_size) 
+# get the target blocks embeddings in the shape of (batch_size, target_block_num, block_size, embedding_size)
+target_blocks_embeddings = torch.stack([target_embeddings[index,target_block_range,:] for index, target_block_range in enumerate(target_block_indices)]) # get the target embeddings
 #%%
-# target_blocks_embeddings = target_embeddings[target_block_ranges.view(target_embeddings.shape[0], target_block_num, block_size), :, :] # TODO: probably doesn't work for 1 < dimensions get the target embeddings
-# #%%
-# target_blocks_embeddings = target_embeddings[:,target_block_ranges,:] # TODO: probably doesn't work for 1 < dimensions get the target embeddings
-# target_blocks_embeddings.shape
-#%%
-for index, sample_range in enumerate(target_block_ranges):
+for index, sample_range in enumerate(target_block_indices):
     for jndex, _range in enumerate(sample_range):
         print(index, jndex, _range)
         assert torch.all(target_embeddings[index, _range, :] == target_blocks_embeddings[index, jndex, :])
 
-#%%
-target_block_ranges.shape
 
-#%%
-target_block_ranges.view(target_embeddings.shape[0], -1).shape
 
 #%%
 allowed_in_context = tokenized['attention_mask'].bool() # create a tensor of trues
-for index, target_block_range in enumerate(target_block_ranges.view(target_embeddings.shape[0], -1)):
+for index, target_block_range in enumerate(target_block_indices.view(target_embeddings.shape[0], -1)):
     allowed_in_context[index,target_block_range] = False # set the indices of the target blocks to false
-
-
-print(allowed_in_context)
-print(allowed_in_context.shape)
 
 #%%
 # make sure all context blocks have the same length
@@ -184,67 +170,36 @@ context_blocks_indices = []
 for index, input_to_remove in enumerate(inputs_to_remove): # for each sample
     context_block_indices = torch.arange(0, target_embeddings.shape[1])[allowed_in_context[index]] # get the indices of the context inputs
     perm = torch.randperm(context_block_indices.size(0)) # shuffle the indices
-    idx = perm[:smallest_context_length] # select smallest_context_length indices
+    idx = perm[:smallest_context_length] # select indices up to the smallest context length
     context_blocks_indices.append(context_block_indices[idx]) # add the indices to the list
 
 context_blocks_indices = torch.stack(context_blocks_indices)
-#%%
 
 #%%
 context_blocks_embeddings = context_encoder(torch.gather(tokenized['input_ids'], 1, context_blocks_indices), id_indices=context_blocks_indices) 
 
-#%%
-context_blocks_embeddings.shape
-
-#%%
-target_embeddings.shape
 
 #%%
 # predict target blocks from context blocks
-mask_token_embedding = F.normalize(torch.ones(small_encoder_config.n_embd), dim = 0) # initialize the mask token embedding
+mask_token_embedding = F.normalize(torch.ones(small_encoder_config.n_embd), dim = 0) #TODO: replace dummy embedding, initialize the mask token embedding
 mask_token_embedding.shape
 
 #%%
 prediction_embeddings = mask_token_embedding.repeat(target_block_num * context_blocks_embeddings.shape[0], block_size, 1) # for the number of target blocks, for the number of targets per block, repeat the mask token 
 context_embeddings = context_blocks_embeddings.repeat(target_block_num, 1, 1) # for the number of target blocks, repeat the context embeddings
-context_embeddings.shape
-
-#%%
-
-#%%
-context_blocks_indices.shape
-
-#%%
-prediction_embeddings.shape
-
-#%%
 input_embeddings = torch.cat((context_embeddings, prediction_embeddings), dim = 1) # concatenate the context and prediction embeddings
-input_embeddings.shape
-#%%
-r_context_block_indices = context_blocks_indices.repeat(target_block_num, 1) # for the number of target blocks, repeat the context indices
-r_context_block_indices.shape
 
 #%%
-target_block_ranges.view(context_embeddings.shape[0], -1).shape
+input_indices = torch.cat((context_blocks_indices.repeat(target_block_num, 1), target_block_indices.view(context_embeddings.shape[0], -1)), dim = 1) # concatenate the context and target indices
 
-#%%
-input_indices = torch.cat((r_context_block_indices, target_block_ranges.view(context_embeddings.shape[0], -1)), dim = 1) # concatenate the context and target indices
-
-all_predictions = predictor(input_embeddings, input_indices) # predict the target embeddings
+predicted_embeddings = predictor(input_embeddings, input_indices) # predict the target embeddings
 
 #%%
 _, context_length, _ = context_embeddings.shape
-
-masked_predictions = all_predictions[:,context_length:] # remove the context predictions
-
-#%%
-masked_predictions.shape
-
-#%%
-target_blocks_embeddings.shape
+masked_embeddings = predicted_embeddings[:,context_length:] # remove the context predictions
 
 #%%
 # compute the loss of the masked predictions
-loss = F.smooth_l1_loss(masked_predictions, target_blocks_embeddings.view(masked_predictions.shape)) # compute the loss
+loss = F.smooth_l1_loss(masked_embeddings, target_blocks_embeddings.view(masked_embeddings.shape)) # compute the loss
 
 #%%
