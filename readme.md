cc7b41af19dbf753627228780cb33d2a056ddebb
Alex Speicher <alexanderspeicher@gmail.com>
2023-12-17 21:19:34 -0600

    feat: resume training

---
+++ readme.md
@@ -0,0 +1,19 @@
+# TODOs
+
+## Short term
+
+- [X] Device
+- [X] Save/Load models
+- [ ] Save/Load optimizer & schedulers
+- [ ] Log training info
+- [ ] Training runs
+- [ ] Target Packing
+- [ ] UL2 targets
+- [ ] Context/Input Packing
+
+## Datasets
+
+# TinyStories
+
+max token length with LLaMa tokenizer: 1124
+
--- train.py
+++ train.py
@@ -2,6 +2,14 @@
 import copy
 
 from dataclasses import dataclass
+import math
+import os
+import json
+from contextlib import nullcontext
+import random
+import shutil
+import sys
+import numpy as np
 from torch.utils.data import DataLoader
 from schedulers import CosineWDSchedule, WarmupCosineSchedule
 from src.models.encoder import Encoder, EncoderConfig, Predictor, PredictorConfig
@@ -11,6 +19,17 @@ from tqdm import tqdm
 
 import torch
 import torch.nn.functional as F
+
+#%%
+def set_seed(seed):
+    random.seed(seed)
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+def seed_worker(worker_id):
+    worker_seed = torch.initial_seed() % 2**32
+    np.random.seed(worker_seed)
+    random.seed(worker_seed)
 
 #%%
 encoder_config = EncoderConfig(
@@ -43,31 +62,81 @@ predictor_config = PredictorConfig(
 )
 
 #%%
+init_from = "resume"
+init_from = "scratch"
+init_from = init_from if not sys.argv[1:] else sys.argv[1]
+
+print(f"init_from: {init_from}")
+
+out_dir = "out"
+
+max_iternum = 100 if not sys.argv[2:] else int(sys.argv[2])
+
+os.makedirs(out_dir, exist_ok=True)
+
+context_encoder_path = os.path.join(out_dir, "context_encoder.pt")
+target_encoder_path = os.path.join(out_dir, "target_encoder.pt")
+predictor_path = os.path.join(out_dir, "predictor.pt")
+optimizer_path = os.path.join(out_dir, "optimizer.pt")
+
+#%%
 max_context_mask_ratio = 0.8#1.0 # how much of the input should be included in the context
 max_target_mask_ratio = .25# how much of the input should be used for targets
 target_block_num = 4
+
+batch_size = 40
+accumulation_steps = 1
+eval_interval = 50
+
+random_seed = 42
+
+#%%
+set_seed(random_seed)
+
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+#%%
+if init_from == "scratch":
+    context_encoder = Encoder(small_encoder_config) # initialize context encoder
+    target_encoder = copy.deepcopy(context_encoder) # create target encoder as a copy of context encoder
+    # freeze target encoder
+    for param in target_encoder.parameters():
+        param.requires_grad = False
+
+    predictor = Predictor(predictor_config) # initialize predictor
+
+    if os.path.exists(out_dir):
+        shutil.rmtree(out_dir)
+    os.makedirs(out_dir, exist_ok=True)
+    
+elif init_from == "resume":
+    context_encoder = Encoder(small_encoder_config)
+    target_encoder = Encoder(small_encoder_config)
+    predictor = Predictor(predictor_config)
+
+    context_encoder.load_state_dict(torch.load(context_encoder_path))
+    target_encoder.load_state_dict(torch.load(target_encoder_path))
+    predictor.load_state_dict(torch.load(predictor_path))
+
+    optimizer = torch.optim.AdamW(context_encoder.parameters())
+
+    train_run_data = torch.load(os.path.join(out_dir, 'train_run_save_dict.pt'))
+
+    iter_num = train_run_data['iter_num']
+
+    # freeze target encoder
+    for param in target_encoder.parameters():
+        param.requires_grad = False
 
 
 #%%
 tokenizer = LlamaTokenizer.from_pretrained('llama_tokenizer', use_fast = False) # initialize tokenizer
 tokenizer.pad_token = tokenizer.eos_token
 
-context_encoder = Encoder(small_encoder_config) # initialize context encoder
-target_encoder = copy.deepcopy(context_encoder) # create target encoder as a copy of context encoder
-# freeze target encoder
-for param in target_encoder.parameters():
-    param.requires_grad = False
-predictor = Predictor(predictor_config) # initialize predictor
-
-
-
 #%%
+#TODO: move to prep data script 
 dataset = load_from_disk('data/TinyStories').filter(lambda x: tokenizer(x['text'], return_tensors="pt")['input_ids'].shape[1] > 8, num_proc=12)
-
-
-#%%
-train_dataloader = DataLoader(dataset['train'], batch_size = 4, shuffle = True)
-
 
 #%%
 @dataclass
@@ -88,7 +157,7 @@ opt_config = OptimizerConfig()
 # init momentum scheduler
 ema = opt_config.ema#opt_config.ema
 ipe_scale = opt_config.ipe_scale
-iterations_per_epoch = len(dataset['train'])
+iterations_per_epoch = math.ceil(len(dataset['train']) / batch_size) # TODO: add .floor if the last batch should be dropped
 num_epochs = opt_config.num_epochs
 final_lr = opt_config.final_lr
 final_wd = opt_config.final_weight_decay
@@ -97,12 +166,20 @@ start_lr = opt_config.start_lr
 warmup = opt_config.warmup
 wd = opt_config.weight_decay
 
-batch_size = 1
+#%%
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device_type = 'cuda' if 'cuda' in device else 'cpu'
+
+assert device_type == 'cuda', 'CPU training is not supported'
 
 dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32', 'bfloat16', or 'float16', the latter will auto implement a GradScaler
-use_bfloat16 = False
-
-
+ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[dtype]
+type_casting = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
+#%%
+# load the models to the device
+context_encoder.to(device)
+target_encoder.to(device)
+predictor.to(device)
 
 #%%
 
@@ -126,35 +203,62 @@ param_groups = [
         }
     ]
 
+iter_num = 0 if init_from == "scratch" else train_run_data['iter_num']
+
 optimizer = torch.optim.AdamW(param_groups)
-scheduler = WarmupCosineSchedule(
+if init_from == "resume":
+    optimizer.load_state_dict(torch.load(optimizer_path))
+
+lr_scheduler = WarmupCosineSchedule(
     optimizer,
     warmup_steps=int(warmup*iterations_per_epoch),
     start_lr=start_lr,
     ref_lr=lr,
     final_lr=final_lr,
-    T_max=int(ipe_scale*num_epochs*iterations_per_epoch))
+    T_max=int(ipe_scale*num_epochs*iterations_per_epoch),
+    step=iter_num
+)
+
 wd_scheduler = CosineWDSchedule(
     optimizer,
     ref_wd=wd,
     final_wd=final_wd,
-    T_max=int(ipe_scale*num_epochs*iterations_per_epoch))
-scaler = torch.cuda.amp.GradScaler() if use_bfloat16 else None
+    T_max=int(ipe_scale*num_epochs*iterations_per_epoch),
+    step=iter_num
+)
+
+# initialize a GradScaler. If enabled=False scaler is a no-op
+scaler = torch.cuda.amp.GradScaler(enabled=(dtype == 'float16'))
 
 momentum_scheduler = (ema[0] + i*(ema[1]-ema[0])/(iterations_per_epoch*num_epochs*ipe_scale)
-                          for i in range(int(iterations_per_epoch*num_epochs*ipe_scale)+1))
+                          for i in range(iter_num, int(iterations_per_epoch*num_epochs*ipe_scale)+1))
 
 #%%
-for epoch in range(1, num_epochs + 1):
-    for batch in train_dataloader:
-        tokenized = tokenizer(batch['text'], padding = True, truncation = False, max_length = 1024, return_tensors="pt")
+def get_batch(split, index, batch_size):
+    data = dataset[split]
+    return data[index:index+batch_size]
 
-        batch_count = tokenized['input_ids'].shape[0]
-        input_length = tokenized['input_ids'].shape[1]
-        prediction_count = batch_count * target_block_num
+#%%
+best_loss = 1e9
+while iter_num < max_iternum:
+    set_seed(random_seed) #TODO: find better solution for reproducibility?
+    epoch = iter_num // len(dataset["train"])
 
+    batch_idx = iter_num % math.ceil(len(dataset["train"]) / batch_size)
+    batch = get_batch('train', batch_idx, min(batch_size, len(dataset["train"]) - batch_idx * batch_size))
+    
+    with open(os.path.join(out_dir, 'batch.jsonl'), 'a') as f:
+        f.write(json.dumps({'text': batch['text']}) + '\n')
+
+    tokenized = tokenizer(batch['text'], padding = True, truncation = False, max_length = 1024, return_tensors="pt")
+
+    batch_count = tokenized['input_ids'].shape[0]
+    input_length = tokenized['input_ids'].shape[1]
+    prediction_count = batch_count * target_block_num
+
+    with type_casting:
         # compute the target embeddings
-        target_embeddings = target_encoder(tokenized['input_ids']) # get target embeddings, no need to provide input indices.
+        target_embeddings = target_encoder(tokenized['input_ids'].to(device)) # get target embeddings, no need to provide input indices.
         #target_embeddings = F.layer_norm(target_embeddings, (target_embeddings.size(-1),))  #TODO: may not be required normalize over feature-dim
         #TODO: normalize the target embeddings?
 
@@ -208,7 +312,7 @@ for epoch in range(1, num_epochs + 1):
             context_attention_mask[index, :allowed_context_length] = True # set the attention mask to true for the context inputs
 
         # get the context blocks embeddings
-        context_blocks_embeddings = context_encoder(context_inputs, id_indices=context_blocks_indices, attn_mask=context_attention_mask.unsqueeze(1).unsqueeze(1))
+        context_blocks_embeddings = context_encoder(context_inputs.to(device), id_indices=context_blocks_indices.to(device), attn_mask=context_attention_mask.unsqueeze(1).unsqueeze(1).to(device))
 
         # create the prediction attention mask
         prediction_input_size = max_context_length + max_target_block_size 
@@ -220,7 +324,7 @@ for epoch in range(1, num_epochs + 1):
         pred_context_attn_mask = pred_context_attn_mask.view(batch_count, prediction_input_size,  prediction_input_size)
         # create the target attention mask
         pred_target_attn_mask = torch.zeros((batch_count, prediction_input_size), dtype = torch.bool)
-        # let the target blocks attend to each other
+        # let the tokens in each target block attend to each other
         for index, block_size in enumerate(target_block_sizes):
             pred_target_attn_mask[index, max_context_length:max_context_length+block_size] = True
 
@@ -234,39 +338,73 @@ for epoch in range(1, num_epochs + 1):
         # mask_toke_embeddings are the same so we can just repeat them
         prediction_embeddings = mask_token_embedding.repeat(target_block_num * batch_count, max_target_block_size, 1) # for the number of target blocks, for the number of targets per block, repeat the mask token 
         context_embeddings = context_blocks_embeddings.repeat_interleave(target_block_num, dim = 0) #.repeat(target_block_num, 1, 1) # for the number of target blocks, repeat the context embeddings
-        input_embeddings = torch.cat((context_embeddings, prediction_embeddings), dim = 1) # concatenate the context and prediction embeddings
+        input_embeddings = torch.cat((context_embeddings, prediction_embeddings.to(device)), dim = 1) # concatenate the context and prediction embeddings
 
         input_indices = torch.cat((context_blocks_indices.repeat_interleave(target_block_num, dim = 0), target_block_indices.view(prediction_count, -1)), dim = 1) # concatenate the context and target indices
 
-        predicted_embeddings = predictor(input_embeddings, input_indices, attn_mask=prediction_attn_mask.unsqueeze(1)) # predict the target embeddings
+        predicted_embeddings = predictor(input_embeddings, input_indices.to(device), attn_mask=prediction_attn_mask.unsqueeze(1).to(device)) # predict the target embeddings
 
         _, context_length, _ = context_embeddings.shape
         predicted_target_embeddings = predicted_embeddings[:,context_length:] # remove the context predictions
 
         # for each sample in the batch, mask out the target blocks which are irrelevant
         relevant_target_attn_mask = pred_target_attn_mask[:, context_length:].repeat_interleave(target_block_num, dim = 0).unsqueeze(2)
+
+        relevant_target_attn_mask = relevant_target_attn_mask.to(device)
+
         # compute the loss of the masked predictions
         loss = F.smooth_l1_loss(predicted_target_embeddings * relevant_target_attn_mask, target_blocks_embeddings.view(predicted_target_embeddings.shape) * relevant_target_attn_mask) # compute the loss
 
-        print(loss.item())
-        # break if loss is nan
-        assert not torch.isnan(loss), 'loss is nan!'
+
+    # print(loss.item(), iter_num)
+    # break if loss is nan
+    assert not torch.isnan(loss), 'loss is nan!'
+
+    # if loss.item() < best_loss and iter_num % eval_interval == 0:
+    # TODO: change to evaluation
+    if True:
+        best_loss = loss.item()
+        torch.save(context_encoder.state_dict(), context_encoder_path)
+        torch.save(target_encoder.state_dict(), target_encoder_path)
+        torch.save(predictor.state_dict(), predictor_path)
+        torch.save(optimizer.state_dict(), optimizer_path)
+
+        train_run_save_dict = {
+                'iter_num': iter_num,
+                'epoch': epoch,
+                'batch_idx': batch_idx,
+                'loss': loss.item(),
+                'batch_size': batch_size,
+                'torch_seed': torch.initial_seed(),
+                # 'scaler': None if scaler is None else scaler.state_dict(), # not needed
+                'lr': lr
+            }
+
+        torch.save(train_run_save_dict, os.path.join(out_dir, 'train_run_save_dict.pt'))
+
+    scaler.scale(loss).backward()
+    # TODO: add gradient accumulation
+    scaler.step(optimizer)
+    scaler.update()
+
+    optimizer.zero_grad(set_to_none=True)
+
+    _new_lr = lr_scheduler.step()
+    _new_wd = wd_scheduler.step()
 
 
-        if use_bfloat16:
-            scaler.scale(loss).backward()
-            scaler.step(optimizer)
-            scaler.update()
-        else:
-            loss.backward()
-            optimizer.step()
-        # grad_stats = grad_logger(encoder.named_parameters())
-        optimizer.zero_grad()
+    with torch.no_grad():
+        m = next(momentum_scheduler)
+        for param_q, param_k in zip(context_encoder.parameters(), target_encoder.parameters()):
+            param_k.data.mul_(m).add_((1.-m) * param_q.detach().data)
 
-        with torch.no_grad():
-            m = next(momentum_scheduler)
-            for param_q, param_k in zip(context_encoder.parameters(), target_encoder.parameters()):
-                param_k.data.mul_(m).add_((1.-m) * param_q.detach().data)
+    print(target_block_start_indices)
+    print(f'lr: {_new_lr}, wd: {_new_wd} m: {m} lss: {loss.item()} iter_num: {iter_num}')
+
+    with open(os.path.join(out_dir, 'losses.jsonl'), 'a') as f:
+        f.write(json.dumps({'loss': loss.item(), 'iter_num' : iter_num}) + '\n')
+
+    iter_num += 1
 
 
 #%%
